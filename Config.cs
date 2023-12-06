5ba8cd862cef11acda0205822e9aaeee5827844a
Natesworks <github@natesworks.com>
2023-12-06 16:26:03 -0500

    Add temporary Config.cs

---
+++ Config.cs
@@ -0,0 +1,21 @@
+/*
+using System;
+using Newtonsoft.Json;
+using System.IO;
+
+class Config
+{
+    static void Config()
+    {
+         string ConfigPath = "test.json";
+
+         string Content = File.ReadAllText(jsonFilePath);
+
+         dynamic Data = JsonConvert.DeserializeObject(jsonContent);
+
+         string Value = jsonData.hi;
+
+         Console.WriteLine(Value);
+    }
+}
+*/
